83e02385f1370a42c1001beea037134ed400fb82
Alexander Bai <cellfoneguy@gmail.com>
2020-03-28 13:08:21 -0500

    added stuff

---
+++ react/app/entry.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Featured
+import Index from "./views/Index";
+import About from "./views/About";
+import Projects from "./views/Projects";
+import NotFound from "./views/NotFound";
+
+// All of our CSS
+import "./static/css/main.scss";
+
+ReactDOM.render(
+  <Router basename={BASE_PATH}>
+    <Switch>
+      <Route exact path="/" component={Index} />
+      <Route path="/about" component={About} />
+      <Route path="/projects" component={Projects} />
+      {/* Only useful in development mode */}
+      <Route component={NotFound} status={404} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
