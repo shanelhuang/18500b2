45f55439dffe8c60abca6e0ef057b5e342f3b6df
Alexander Bai <cellfoneguy@gmail.com>
2020-03-28 16:24:44 -0500

    shitty search done

--- nav/compress.py
+++ nav/compress.py
@@ -6,8 +6,28 @@ from pgm_utils import pgm_save
 MAP_SIZE = 800 # pixel size of input
 CHUNK_SIZE = 10 # compression factor
 MAX_SEARCH = 5 # max chunks outward to search
-SEARCH_ROW = 39 # row, col to begin the nav plan (array starts at 0)
-SEARCH_COL = 34
+SEARCH_ROW = 40 # row, col to begin the nav plan (array starts at 0)
+SEARCH_COL = 35
+
+
+
+def search():
+	for row_offset in range(-MAX_SEARCH - 1, MAX_SEARCH):
+		cur_row = SEARCH_ROW + row_offset
+		if(cur_row < 0 or cur_row >= num_chunks):
+			continue
+		for col_offset in range(-MAX_SEARCH - 1, MAX_SEARCH):
+			cur_col = SEARCH_COL + col_offset
+			if(cur_col < 0 or cur_col >= num_chunks):
+				continue
+			if(compressed_map_2d[cur_row][cur_col] > 0 and compressed_map_2d[cur_row][cur_col] < 200):
+				return cur_row, cur_col
+	return None
+
+
+
+
+
 
 # read in binary file
 dirname = os.path.dirname(__file__)
@@ -34,18 +54,27 @@ for chunk_row in range(num_chunks):
 		compressed_map[(chunk_row * num_chunks) + chunk_col] = avg
 		compressed_map_2d[chunk_row][chunk_col] = avg
 
-# approx middle pixel [39][34]
+# approx middle pixel
 compressed_map[(SEARCH_ROW) * 80 + SEARCH_COL] = 0
 
 # expanding radial search outward from given point
-row = 39
-col = 34
-for row_offset in range(-MAX_SEARCH - 1, MAX_SEARCH):
-	for col_offset in range(-MAX_SEARCH -1, MAX_SEARCH):
-		
+outuple = search()
+if(outuple is not None):
+	(dest_row, dest_col) = outuple
+	print(dest_row, dest_col)
+	compressed_map[dest_row * num_chunks + dest_col] = 0
+else:
+	print("None found")
+
+
 
 
 #print(compressed_map)
 pgm_save('compressed_map.pgm', compressed_map, (num_chunks, num_chunks))
+
+
+
+
+
 
 
