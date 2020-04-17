db96d1e958bef87c2b42c4f8a92d0e48b7032bb7
Aditi <adhebbar@gmail.com>
2020-04-16 20:00:32 -0400

    lil code

--- os/sensors.py
+++ os/sensors.py
@@ -1,5 +1,20 @@
+import create2api
+import json   # We'll use this to format the output
 
 
-def monitor(foundObstacle):
+
+def monitor(currentProgram, bot):
+	json.dumps(bot.sensor_state, indent=4)
+	bot.get_packet(100)
 	while True:
-		foundObstacle[0] = True
+		print(json.dumps(bot.sensor_state, indent=4, sort_keys=False))
+		currentProgram.foundObstacle = True
+
+# bot = create2api.Create2()
+# bot.start()
+# bot.safe()
+# json.dumps(bot.sensor_state, indent=4)
+# bot.get_packet(100)
+# while True:
+# 	print(json.dumps(bot.sensor_state, indent=4, sort_keys=False))
+# 	foundObstacle[0] = True
